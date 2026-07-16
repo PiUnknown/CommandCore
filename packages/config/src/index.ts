f787ae07bab5ea4a41acaabf71d763eeb1e39a15
PiUnknown <okjha09@gmail.com>
2026-07-16 12:43:36 +0530

    chore(monorepo): create shared workspace packages

---
+++ packages/config/src/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = "CommandCore";
+
+export const APP_DESCRIPTION =
+  "Your Personal Command Center";
+
+export const DEFAULT_THEME = "dark";
---
+++ packages/types/src/index.ts
@@ -0,0 +1,13 @@
+export type WidgetId =
+  | "ai"
+  | "mail"
+  | "calendar"
+  | "spotify"
+  | "news"
+  | "todo"
+  | "terminal";
+
+export type WidgetState =
+  | "normal"
+  | "expanded"
+  | "minimized";
